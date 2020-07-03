12f077385f6e03feb7d71cb4bdf849bf4fa66b54
Holger Stitz <holger.stitz@datavisyn.io>
2020-07-03 18:56:51 +0200

    Add interfaces for bar chart onboarding spec

--- packages/onboarding-core/src/index.ts
+++ packages/onboarding-core/src/index.ts
@@ -1,3 +1,4 @@
+export * from './interfaces';
 
 export default function logger(message: string) {
   console.log(message);
---
+++ packages/onboarding-core/src/interfaces.ts
@@ -0,0 +1,42 @@
+
+
+export interface OnboardingMessages {
+  anchor: any;
+  requires: string[];
+  legend: string;
+}
+
+interface OnboardingCoordsAnchor {
+  sel: string;
+  coords: number[];
+}
+
+interface OnboardingDOMAnchor {
+  sel: string;
+  useDOMRect?: boolean;
+}
+
+interface SpecProp {
+  value: any;
+  anchor?: OnboardingCoordsAnchor | OnboardingDOMAnchor;
+}
+
+interface OnboardingSpec {
+  [key: string]: SpecProp
+}
+
+export interface OnboardingBarChartSpec extends OnboardingSpec {
+  chartTitle: SpecProp;
+  type: SpecProp;
+  orientation: SpecProp;
+  xAxisOrientation: SpecProp;
+  yAxisOrientation: SpecProp;
+  barLength: SpecProp;
+  xMin: SpecProp;
+  xMax: SpecProp;
+  yMin: SpecProp;
+  yMax: SpecProp;
+  xAxisTitle: SpecProp;
+  yAxisTitle: SpecProp;
+}
+
