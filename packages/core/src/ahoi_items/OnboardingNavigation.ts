0e08572eacbf8e3cf7a4e0b3b2195a44b66a0c64
Daniela <daniela.moitzi@datavisyn.io>
2021-11-30 15:32:12 +0100

    remove not used files

--- packages/core/src/ahoi_items/OnboardingNavigation.ts
+++
@@ -1,9 +0,0 @@
-import { IOnboardingMessage } from "../interfaces";
-
-export type NavigationAlignment = "row" | "column";
-
-export default class OnboardingNavigation {
-  constructor(onboardingMessages: IOnboardingMessage[], alignment: NavigationAlignment) {
-
-  }
-}
