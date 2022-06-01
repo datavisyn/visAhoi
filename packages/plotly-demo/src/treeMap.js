1d8853c50181238bee4e2af1cbc368e4f77bfd1f
dvsilviyageo <104566246+dvsilviyageo@users.noreply.github.com>
2022-06-01 11:11:29 +0200

    Added treemap to plotly demo (#92)

---
+++ packages/plotly-demo/src/treeMap.js
@@ -0,0 +1,58 @@
+import Plotly from 'plotly.js-dist';
+import debounce from "lodash.debounce";
+import { importCsv } from './util';
+
+let chart = null;
+let onboardingUI = null;
+
+const debouncedResize = debounce((event) => {
+  onboardingUI?.updateOnboarding(getAhoiConfig())
+}, 250);
+
+const render = async () => {
+const data = await importCsv("./data/jobsPlan.csv");  
+  const {label, parent, value, color} = processData(data);
+  chart = await makePlotly(label, parent, value, color);
+  window.addEventListener('resize', debouncedResize);
+}
+
+const processData = (data) => {
+    const label = [];
+    const parent = [];
+    const value = [];
+    const color = [];
+
+    data.map((d) => {
+        label.push(d.Label);
+        parent.push(d.Parent);
+        value.push(d.Value);
+        color.push(d.Color)
+    });
+
+  return {label, parent, value, color};
+}
+
+const makePlotly = (label, parent, value, color) => {
+    const traces = [
+        {
+            type: 'treemap',
+            labels: label,
+            parents: parent,
+            values: value,
+            marker: {
+                colors: color,
+                // colorscale: 'Greys',
+                      
+            },          
+        }
+    ];
+    const config = {
+        responsive: true
+    };
+    const layout = {
+        title: 'Jobs Plan'
+    };
+    return Plotly.newPlot('vis', traces, layout, config)
+}
+
+render();
