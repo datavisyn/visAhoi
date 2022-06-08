a1dcb1879c3f081460ca4859491bc8a2c7942e26
Silviya Geo <silviya.geo@datavisyn.io>
2022-06-08 14:20:13 +0200

    In switch case stament, break is added to each case

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -32,26 +32,32 @@ export function generateMessages(
         <IOnboardingBarChartSpec>spec,
         visElement
       );
+      break;
 
     case EVisualizationType.CHANGE_MATRIX:
       messages = changeMatrix.generateMessages(
         <IOnboardingChangeMatrixSpec>spec,
         visElement
       );
+      break;
 
     case EVisualizationType.HORIZON_GRAPH:
       messages = horizonGraph.generateMessages(
         <IOnboardingHorizonGraphSpec>spec,
         visElement
       );
+      break;
 
     case EVisualizationType.SCATTERPLOT:
     messages = scatterplot.generateMessages(
       <IOnboardingScatterplotSpec>spec,
       visElement
     );
+    break;
+
     case EVisualizationType.TREEMAP:
       messages = treemap.generateMessages(<IOnboardingTreemapSpec>spec, visElement);
+      break;
   }
   onboardingStages.set([...new Set(messages.map((m) => m.onboardingStage))]);
   return messages;
