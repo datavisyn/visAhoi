5656351649244b2dbf49d82d3cae058cd2fab9f0
Silviya Geo <silviya.geo@datavisyn.io>
2022-08-26 13:20:14 +0200

    deleteOnboardingMessage in tooltip is changed to for only the active onboarding stage

--- packages/core/src/components/NavigationMarker.svelte
+++ packages/core/src/components/NavigationMarker.svelte
@@ -19,7 +19,6 @@
   const { activeBackgroundColor, hoverBackgroundColor, backgroundColor } =
     markerInformation.message.onboardingStage;
   const { marker } = markerInformation;
-  $: console.log(markerInformation, "Marker information");
 
   let arrValue: IMarkerInformation[] = [];
 
