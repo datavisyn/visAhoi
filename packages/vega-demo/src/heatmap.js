d01f71b7debca4ccaa0dd66fdb6aa7269c7bd0d4
Silviya Geo <silviya.geo@datavisyn.io>
2022-09-12 09:20:49 +0200

    added heatmap to vega-lite

--- packages/vega-demo/src/heatmap.js
+++ packages/vega-demo/src/heatmap.js
@@ -18,14 +18,20 @@ const opt = {
   autosize: 'none',
 
   data: { url: 'data/carsData.json' },
-  mark: 'rect',
+  mark: { type: 'rect', tooltip: true },
   encoding: {
     y: { field: 'Origin', type: 'nominal', title: 'Origin' },
     x: { field: 'Cylinders', type: 'ordinal', title: 'Cylinders' },
-    color: { aggregate: 'mean', field: 'Horsepower' }
+    // color: { aggregate: 'mean', field: 'Horsepower' }
+    color: {
+      field: 'Horsepower',
+      aggregate: 'max',
+      type: 'quantitative',
+      legend: { title: null }
+    }
   },
   config: {
-    axis: { grid: true }
+    axis: { grid: true, labels: true }
   }
 
 }
