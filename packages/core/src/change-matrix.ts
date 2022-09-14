19c65c7f0d5c01371fb6d37894df474af0ea658b
Silviya Geo <silviya.geo@datavisyn.io>
2022-09-14 10:08:39 +0200

    added draggable component

--- packages/core/src/change-matrix.ts
+++ packages/core/src/change-matrix.ts
@@ -5,9 +5,9 @@ import {
   defaultOnboardingStages,
   EDefaultOnboardingStages,
   IOnboardingStage,
-  TooltipPosition,
-} from "./interfaces";
-import { getAnchor } from "./utils";
+  TooltipPosition
+} from './interfaces'
+import { getAnchor } from './utils'
 
 export interface IOnboardingChangeMatrixSpec extends IOnboardingSpec {
   chartTitle?: ISpecProp;
@@ -17,79 +17,79 @@ export interface IOnboardingChangeMatrixSpec extends IOnboardingSpec {
   yAxis?: ISpecProp;
 }
 
-function generateMessages(
+function generateMessages (
   spec: IOnboardingChangeMatrixSpec,
   visElement: Element
 ): IOnboardingMessage[] {
   const reading = defaultOnboardingStages.get(
     EDefaultOnboardingStages.READING
-  ) as IOnboardingStage;
+  ) as IOnboardingStage
 
   const messages: IOnboardingMessage[] = [
     {
       anchor: getAnchor(spec.type, visElement),
-      requires: ["type"],
+      requires: ['type'],
       text: `The chart Is based on colored ${spec.type?.value} elements.`,
-      title: "Reading the chart",
+      title: 'Reading the chart',
       onboardingStage: reading,
-      tooltipPosition: "left" as TooltipPosition,
+      tooltipPosition: 'left' as TooltipPosition,
       marker: {
-        id: "unique-marker-id-2",
+        id: 'unique-marker-id-2'
       },
-      id: "unique-message-id-2",
-      order: 2,
+      id: 'unique-message-id-2',
+      order: 2
     },
     {
       anchor: getAnchor(spec.legendTitle, visElement),
-      requires: ["legendTitle"],
+      requires: ['legendTitle'],
       text: `The legend shows the ${spec.legendTitle?.value} for the chart. The colors range from blue to white and brown.`,
-      title: "Reading the chart",
+      title: 'Reading the chart',
       onboardingStage: reading,
       marker: {
-        id: "unique-marker-id-3",
+        id: 'unique-marker-id-3'
       },
-      id: "unique-message-id-3",
-      order: 3,
+      id: 'unique-message-id-3',
+      order: 3
     },
     {
       anchor: getAnchor(spec.xAxis, visElement),
-      requires: ["xAxis", "yAxis"],
+      requires: ['xAxis', 'yAxis'],
       text: `The columns show the ${spec.xAxis?.value}, while the rows show the ${spec.yAxis?.value}.`,
-      title: "Reading the chart",
+      title: 'Reading the chart',
       onboardingStage: reading,
       marker: {
-        id: "unique-marker-id-4",
+        id: 'unique-marker-id-4'
       },
-      id: "unique-message-id-4",
-      order: 4,
-    },
-  ];
+      id: 'unique-message-id-4',
+      order: 4
+    }
+  ]
 
   if (spec.chartTitle?.value !== undefined) {
     messages.unshift({
       anchor: getAnchor(spec.chartTitle, visElement),
-      requires: ["chartTitle"],
+      requires: ['chartTitle'],
       text: `The <span class="visahoi-tooltip-hover-text">chart</span> shows the ${spec.chartTitle?.value}.`,
-      title: "Reading the chart",
+      title: 'Reading the chart',
       onboardingStage: reading,
-      tooltipPosition: "top" as TooltipPosition, // this causes the "jumping" of the tooltip when resizing
+      tooltipPosition: 'top' as TooltipPosition, // this causes the "jumping" of the tooltip when resizing
       marker: {
         radius: 8,
-        content: "",
-        fontSize: "20px",
-        id: "unique-marker-id-1",
+        content: '',
+        fontSize: '20px',
+        id: 'unique-marker-id-1'
       },
-      id: "unique-message-id-1",
-      order: 1,
-    });
+      id: 'unique-message-id-1',
+      order: 1
+    })
   }
 
   // Filter for messages where all template variables are available in the spec
   return messages.filter((message) =>
     message.requires.every((tplVars) => spec[tplVars])
-  );
+  )
 }
 
 export const changeMatrix = {
-  generateMessages,
-};
+  generateMessages
+}
---
+++ packages/core/src/components/Draggable.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  export let left = 100;
+  export let top = 100;
+
+  let moving = false;
+
+  function onMouseDown() {
+    moving = true;
+  }
+
+  function onMouseMove(e) {
+    if (moving) {
+      left += e.movementX;
+      top += e.movementY;
+    }
+  }
+
+  function onMouseUp() {
+    moving = false;
+  }
+
+  // 	$: console.log(moving);
+</script>
+
+<section
+  on:mousedown={onMouseDown}
+  style="left: {left}px; top: {top}px;"
+  class="draggable"
+>
+  <slot />
+</section>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
+
+<style>
+  .draggable {
+    width: 200px;
+    user-select: none;
+    cursor: move;
+    border: solid 1px gray;
+    position: absolute;
+  }
+</style>
