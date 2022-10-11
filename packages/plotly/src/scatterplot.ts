7699930f52b73dae532da59cacd344c398b9f53c
dvsilviyageo <silviya.geo@datavisyn.io>
2022-10-11 09:21:35 +0200

    Added the old required codes to scatterplot.ts

--- packages/plotly/src/scatterplot.ts
+++ packages/plotly/src/scatterplot.ts
@@ -6,6 +6,39 @@ import {
 import { IOnboardingScatterplotSpec } from '@visahoi/core/src/scatterplot'
 
 function extractOnboardingSpec (chart: any, coords): IOnboardingScatterplotSpec {
+  const traceNodes = chart?.querySelectorAll('g.points')
+  const areaNodes = traceNodes ? traceNodes[0]?.querySelectorAll('path.point') : null
+  const areaNodesData = areaNodes
+    ? Array.from(areaNodes).map(
+      (point: any) => point.__data__
+    )
+    : null
+
+  const t = areaNodesData ? areaNodesData[0]?.trace : null
+
+  const grid = document
+    .getElementsByClassName('nsewdrag drag')[0]
+    .getBoundingClientRect()
+
+  const points = areaNodes
+    ? (Array.from(areaNodes) as any[]).filter(
+        (point) =>
+          point.getBoundingClientRect().x &&
+      point.getBoundingClientRect().x <= grid.width + grid.x &&
+      point.getBoundingClientRect().y &&
+      point.getBoundingClientRect().y <= grid.height + grid.y
+      )
+    : null
+
+  if (points) {
+    const xVals = points.map((point) => point.getBoundingClientRect().x)
+    const yVals = points.map((point) => point.getBoundingClientRect().y)
+
+    const maxX = Math.max(...xVals)
+    const maxXIndex = xVals.indexOf(maxX)
+    const maxY = yVals[maxXIndex]
+  }
+
   const title = chart?.layout?.title?.text
   const legend = chart.data[0]?.marker?.colorbar?.title?.text
   let newTitle = ''
@@ -28,6 +61,12 @@ function extractOnboardingSpec (chart: any, coords): IOnboardingScatterplotSpec 
     },
     title: {
       value: title
+    },
+    type: {
+      value: t?.type,
+      anchor: {
+        sel: '.points > .point:nth-child(4)'
+      }
     },
     xAxisTitle: {
       value: chart.layout.xaxis?.title.text,
