890102f73a115662270a611f9dd07f5c7227c99f
dvsilviyageo <silviya.geo@datavisyn.io>
2023-01-26 10:20:54 +0100

    WIP

--- packages/core/src/scatterplot.ts
+++ packages/core/src/scatterplot.ts
@@ -8,7 +8,8 @@ import {
 } from './interfaces'
 import { getAnchor } from './utils'
 import { v4 as uuidv4 } from "uuid";
-
+// @ts-ignore
+import chevronUp from "../src/assets/chevron-up-solid.svg";
 export interface IOnboardingScatterplotSpec extends IOnboardingSpec {
   chartTitle?: ISpecProp;
   title?: ISpecProp;
@@ -33,6 +34,7 @@ function createColorRect (color = 'white') {
   return `<div class="colorRect" style="background-color: ${color}"></div>`
 }
 
+const tesst = `<svg viewBox="0 0 1000 1000" class="icon" height="1em" width="1em"><path d="m500 450c-83 0-150-67-150-150 0-83 67-150 150-150 83 0 150 67 150 150 0 83-67 150-150 150z m400 150h-120c-16 0-34 13-39 29l-31 93c-6 15-23 28-40 28h-340c-16 0-34-13-39-28l-31-94c-6-15-23-28-40-28h-120c-55 0-100-45-100-100v-450c0-55 45-100 100-100h800c55 0 100 45 100 100v450c0 55-45 100-100 100z m-400-550c-138 0-250 112-250 250 0 138 112 250 250 250 138 0 250-112 250-250 0-138-112-250-250-250z m365 380c-19 0-35 16-35 35 0 19 16 35 35 35 19 0 35-16 35-35 0-19-16-35-35-35z" transform="matrix(1 0 0 -1 0 850)"></path></svg> `;
 function generateMessages (
   spec: IOnboardingScatterplotSpec,
   visElement: Element
@@ -65,7 +67,7 @@ function generateMessages (
       // basic chart interactions for plotly
       anchor: getAnchor(spec.plotlyModebar, visElement),
       requires: ['plotlyModebar'],
-      text: "",
+      text: tesst,
       title: "Chart interactions",
       onboardingStage: interacting,
       marker: {
