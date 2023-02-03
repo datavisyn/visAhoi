1622953fa182805fcc229d7243f50208ca4c1f5c
dvsilviyageo <silviya.geo@datavisyn.io>
2023-02-03 11:30:43 +0100

    added barchart stories to test

---
+++ packages/demos/plotly/src/barchart.stories.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  
+    import Barchart from './barchart.svelte';  
+    
+  </script>
+  
+  <Meta title="Plotly/Barchart" component={Barchart}/>
+  
+  <Template let:args>
+    <Barchart {...args}>
+  
+    </Barchart>
+  </Template>
+  
+  <Story name="Basic" args={{contextKey: "test"}}/>
+  
+  <!-- <Story name="Square" source args={{rounded: false}}/> -->
+  
+  <!-- Dynamic snippet should be disabled for this story -->
+  <!-- <Story name="Button No Args">
+    <Button>Label</Button>
+  </Story> -->
---
+++ packages/demos/plotly/src/barchart.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+    import "@visahoi/plotly/build/css/main.css";
+    import ResizeObserver from "svelte-resize-observer";
+    import Plotly from "plotly.js-dist";
+    import { onMount, onDestroy } from "svelte";
+    import {
+      generateBasicAnnotations,
+      ahoi,
+      EVisualizationType
+    } from "@visahoi/plotly";   
+  
+    export let contextKey; 
+    
+
+    var trace1 = {
+  x: ['giraffes', 'orangutans', 'monkeys'],
+  y: [20, 14, 23],
+  name: 'SF Zoo',
+  type: 'bar'
+};
+  
+    let onboardingUI;
+    let runtimeObject;
+  
+    const data = [trace1];
+    const layout = {
+      title: "Horsepower and miles per gallon for various cars",
+      xaxis: {
+        title: "Horsepower",
+      },
+      yaxis: {
+        title: "Miles per Gallon",
+      },
+    };
+    const showOnboarding = false;
+  
+    const getAhoiConfig = () => {
+      const defaultOnboardingMessages = generateBasicAnnotations(
+        contextKey,
+        EVisualizationType.BAR_CHART,
+        runtimeObject
+      );
+      const ahoiConfig = {
+        onboardingMessages: defaultOnboardingMessages,
+      };
+  
+      return ahoiConfig;
+    };
+  
+    const onResize = (e) => {
+      if(onboardingUI) {
+        // update onboarding
+        onboardingUI.updateOnboarding(getAhoiConfig(), runtimeObject)
+      }
+      if(runtimeObject) {
+        Plotly.Plots.resize(runtimeObject)
+      }
+    }
+  
+    onMount(async () => {
+      const plotDiv = document.getElementById(contextKey);
+      runtimeObject = await new Plotly.newPlot(plotDiv, data, layout);
+      if(onboardingUI) {
+        onboardingUI.showOnboarding()
+      } else {
+        onboardingUI = await ahoi(
+          contextKey,
+          EVisualizationType.BAR_CHART,
+          runtimeObject,
+          getAhoiConfig()
+        );
+      }
+    });
+  
+    onDestroy(() => {
+      if(onboardingUI) {
+        onboardingUI.removeOnboarding()
+      }
+    })
+  </script>
+  
+  <div id="plotly">
+    <ResizeObserver on:resize={onResize} />
+    
+    <div id={contextKey}><!-- Plotly chart will be drawn inside this DIV --></div>
+  </div>
+  
+  <style>
+    :global(*) {
+      font-family: sans-serif;
+    }
+  </style>
