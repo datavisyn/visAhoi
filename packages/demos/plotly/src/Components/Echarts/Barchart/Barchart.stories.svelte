c0bfcb2eac9748f13efc9c18db02237670ba9be9
dvsilviyageo <silviya.geo@datavisyn.io>
2023-02-17 09:54:48 +0100

    Added echarts to the package json in demos and added barchart svelte file

---
+++ packages/demos/plotly/src/Components/Echarts/Barchart/Barchart.stories.svelte
@@ -0,0 +1,35 @@
+<!-- <script>
+    import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  
+    import Barchart from './Barchart.svelte';  
+    
+  </script>
+  
+  <Meta title="Echarts/Barchart" component={Barchart}/>
+  
+  <Template let:args>
+    <Barchart {...args}>
+  
+    </Barchart>
+  </Template>
+  
+  <Story name="Demo" args={{contextKey: "barchart"}}/>
+  
+   -->
+
+   <script>
+    import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  
+    import Barchart from './Barchart.svelte';  
+    
+  </script>
+  
+  <Meta title="Echarts/Barchart" component={Barchart}/>
+  
+  <Template let:args>
+    <Barchart {...args}>
+  
+    </Barchart>
+  </Template>
+  
+  <Story name="Demo" args={{contextKey: "barchart"}}/>
---
+++ packages/demos/plotly/src/Components/Echarts/Barchart/Barchart.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+import * as echarts from 'echarts';
+const chartDom = document.getElementById('barchart');
+console.log(chartDom, 'Chart dom');
+const myChart = echarts.init(chartDom);
+console.log(myChart, 'Mychart')
+var option;
+let contextKey;
+
+option = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'bar'
+    }
+  ]
+};
+
+option && myChart.setOption(option);
+
+
+</script>
+<div id="echarts">
+  <h1>Echarts Demo</h1>
+  <div id="barchart"> </div>
+</div>
+
+
+
+<style>
+  :global(*) {
+    font-family: sans-serif;
+  }
+</style>
