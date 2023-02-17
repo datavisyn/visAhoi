b2f20a9403429fca07bc3880e91b13d92f746ee3
dvsilviyageo <silviya.geo@datavisyn.io>
2023-02-17 10:40:04 +0100

    Added barchart as svelte file-WIP

--- packages/demos/plotly/src/Components/Echarts/Barchart/Barchart.svelte
+++ packages/demos/plotly/src/Components/Echarts/Barchart/Barchart.svelte
@@ -1,35 +1,61 @@
 <script lang="ts">
 import * as echarts from 'echarts';
-const chartDom = document.getElementById('barchart');
-console.log(chartDom, 'Chart dom');
-const myChart = echarts.init(chartDom);
-console.log(myChart, 'Mychart')
-var option;
+import { onMount, onDestroy } from "svelte";
+
 let contextKey;
 
-option = {
-  xAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      data: [120, 200, 150, 80, 70, 110, 130],
-      type: 'bar'
-    }
-  ]
-};
+const options = {
+    title: {
+      text: 'Average temperature in Oslo, Norway in 2018',
+      left: 'center'
+    },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      name: 'Month',
+      nameLocation: 'middle',
+      nameGap: 30,
+      data: ['2018-01', '2018-02', '2018-03', '2018-04', '2018-05', '2018-06', '2018-07', '2018-08', '2018-09', '2018-10', '2018-11', '2018-12', 'undefined-undefined'],
+      axisLabel: {
+        formatter: function (value) {
+          const date = new Date(value)
+          return date.getMonth() + 1
+        }
+      }
+    },
+    yAxis: {
+      type: 'value',
+      name: 'Average Temperature in °C',
+      nameLocation: 'middle',
+      nameGap: 35
+    },
+    series: [
+      {
+        data: [-2, -4, -2, 6, 16, 18, 22, 16, 12, 7, 3, -1, NaN],
+        type: 'bar',
+        color: 'lightgrey'
+      }
+    ]
+  }
 
-option && myChart.setOption(option);
+
+// option && myChart.setOption(option);
+
+onMount(async () => {
+  const chartDom = document.getElementById('barchart');
+  console.log(chartDom, 'Chart dom');
+  const myChart = echarts.init(chartDom, null, {
+  renderer: 'svg'
+});
+  myChart.setOption(options)
+console.log(myChart, 'Mychart')
+    });
 
 
 </script>
-<div id="echarts">
+<div id="echarts" style="width: 100%; height: 100%;">
   <h1>Echarts Demo</h1>
-  <div id="barchart"> </div>
+  <div id="barchart" style="width: 500px; height: 500px;"> </div>
 </div>
 
 
