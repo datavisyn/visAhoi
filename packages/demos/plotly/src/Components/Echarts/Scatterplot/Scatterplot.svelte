7d8b25bdafdd0e936cf3445ec7801a4e4e8da84b
dvsilviyageo <silviya.geo@datavisyn.io>
2023-02-20 08:45:58 +0100

    Added scatterplot echarts in svelte and storybook

---
+++ packages/demos/plotly/src/Components/Echarts/Scatterplot/Scatterplot.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+    import * as echarts from 'echarts';
+    import { generateBasicAnnotations, ahoi, EVisualizationType } from '@visahoi/echarts'
+    import { onMount, onDestroy } from "svelte";
+    
+    let contextKey = 'horizonGraph';
+    let onboardingUI;    
+
+    const options = {
+    title: {
+      text: 'Horsepower and miles per gallon for various cars',
+      left: 'center'
+    },
+    tooltip: {},
+    xAxis: {
+      type: 'value',
+      name: 'Horsepower',
+      nameLocation: 'middle',
+      nameGap: 30
+    },
+    yAxis: {
+      type: 'value',
+      name: 'Miles per Gallon',
+      nameLocation: 'middle',
+      nameGap: 35
+    },
+    series: [
+      {
+        data: [
+        [10.0, 8.04],
+        [8.07, 6.95],
+        [13.0, 7.58],
+        [9.05, 8.81],
+        [11.0, 8.33],
+        [14.0, 7.66],
+        [13.4, 6.81],
+        [10.0, 6.33],
+        [14.0, 8.96],
+        [12.5, 6.82],
+        [9.15, 7.2],
+        [11.5, 7.2],
+        [3.03, 4.23],
+        [12.2, 7.83],
+        [2.02, 4.47],
+        [1.05, 3.33],
+        [4.05, 4.96],
+        [6.03, 7.24],
+        [12.0, 6.26],
+        [12.0, 8.84],
+        [7.08, 5.82],
+        [5.02, 5.68]
+      ],
+        type: 'scatter',
+        symbolSize: 4
+      }
+    ]
+  }
+
+    const getAhoiConfig = (contextKey, runtimeObject) => {    
+        const defaultOnboardingMessages = generateBasicAnnotations(
+          contextKey,
+          EVisualizationType.SCATTERPLOT,
+          runtimeObject
+        );      
+        const ahoiConfig = {
+          onboardingMessages: defaultOnboardingMessages,
+        };
+        
+        return ahoiConfig;
+    };
+    
+    // option && myChart.setOption(option);
+    
+    onMount(async () => {
+      const chartDom = document.getElementById('scatterplot');  
+      const runtimeObject = echarts.init(chartDom, null, {
+      renderer: 'svg'
+      });
+      runtimeObject.setOption(options);  
+      if(onboardingUI) {
+        onboardingUI.showOnboarding();    
+        } else {      
+          onboardingUI = await ahoi(
+            contextKey,
+            EVisualizationType.SCATTERPLOT,
+            runtimeObject,
+            getAhoiConfig(contextKey, runtimeObject)
+          );
+        }
+      });
+      
+      onDestroy(() => {
+        if(onboardingUI) {
+          onboardingUI.removeOnboarding();
+        }
+      });
+    
+    
+</script>
+    
+     
+      <div id="scatterplot" style="width: 600px; height: 600px;"> </div>
+   
+    
+    
+    <style>
+      :global(*) {
+        font-family: sans-serif;
+      }
+    </style>
+    
