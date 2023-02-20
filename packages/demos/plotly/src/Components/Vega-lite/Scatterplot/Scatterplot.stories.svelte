01d5c129ae990179fde354c06757276b5ac72cb3
dvsilviyageo <silviya.geo@datavisyn.io>
2023-02-20 15:14:09 +0100

    Added scatterplot vega to storybook

---
+++ packages/demos/plotly/src/Components/Vega-lite/Scatterplot/Scatterplot.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Meta, Story, Template } from '@storybook/addon-svelte-csf'; 
+    import Scatterplot from './Scatterplot.svelte';
+  
+    
+  </script>
+  
+  <Meta title="Visahoi/Vega-lite/Scatterplot" component={Scatterplot}/>
+  
+  <Template let:args>
+    <Scatterplot {...args}>
+  
+    </Scatterplot>
+  </Template>
+  
+  <Story name="Demo" args={{contextKey: "scatterplot"}}/>
+  
