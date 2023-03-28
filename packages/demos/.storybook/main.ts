dc44c395b04e37a84376c900c701651c030b9b77
Daniela <daniela.moitzi@datavisyn.io>
2023-03-28 20:44:56 +0200

    tmp

---
+++ packages/demos/.storybook/main.ts
@@ -0,0 +1,13 @@
+import type { StorybookConfig } from "@storybook/svelte-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/svelte-vite",
+    options: {}
+  },
+  docs: {
+    autodocs: "tag"
+  }
+};
+export default config;
